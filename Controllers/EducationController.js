eb64bc190921d715330335b66950ad3f5319808b
altnbsmehmet <altnbsmehmet@icloud.com>
2025-03-24 03:53:50 +0300

    CRUD endpoints added for Education entity.

--- Controllers/EducationController.js
+++ Controllers/EducationController.js
@@ -5,5 +5,39 @@ import authMiddleware from "../middlewares/AuthMiddleware.js";
 const router = express.Router();
 
 
+router.post('/create', authMiddleware, async function CreateEducation(req, res) {
+    req.body.userId = req.user.userId;
+    const creationResponse = await educationService.CreateAsync(req.body);
+    res.json(creationResponse);
+});
+
+
+router.get('/:userId', async function GetAllEducationByUserId(req, res) {
+    const educationRecords = await educationService.GetAllByUserIdAsync(req.params.userId);
+    res.json(educationRecords);
+});
+
+
+router.patch('/update/:educationId', authMiddleware, async function UpdateEducation(req, res) {
+    const data = { 
+        userId: req.user.userId, 
+        educationId: req.params.educationId, 
+        school: req.body.school, 
+        degree: req.body.degree, 
+        description: req.body.description, 
+        startDate: req.body.startDate, 
+        endDate: req.body.endDate 
+    };
+    const updateResponse = await educationService.UpdateAsync(data);
+    res.json(updateResponse);
+});
+
+
+router.delete('/delete/:educationId', authMiddleware, async function DeleteEducation(req, res) {
+    const data = { userId: req.user.userId, educationId: req.params.educationId };
+    const deleteResponse = await educationService.DeleteAsync(data);
+    res.json(deleteResponse);
+});
+
 
 export default router;
--- Services/EducationService.js
+++ Services/EducationService.js
@@ -2,9 +2,44 @@ import { models } from '../data/db.js';
 import { errorHandler } from '../utilities/ErrorHandler.js';
 
 
+const CreateAsync = errorHandler(async function EducationService_CreateAsync(data) {
+    const education = new models.Education(data);
+    await education.save();
+    return { isSuccess: true, message: "Education record created successfully" };
+});
 
+
+const GetAllByUserIdAsync = errorHandler(async function EducationService_GetAllByUserIdAsync(userId) {
+    const educationRecords = await models.Education.find({ userId });
+    return { isSuccess: true, message: "Education records associated with given userId read.", educationRecords };
+});
+
+
+const UpdateAsync = errorHandler(async function EducationService_UpdateAsync(data) {
+    const { userId, educationId, ...updateFields } = data;
+    const updatedEducation = await models.Education.findOneAndUpdate(
+        { _id: educationId, userId },
+        { $set: updateFields },
+        { new: true }
+    );
+
+    if (!updatedEducation) return { isSuccess: false, message: "Education record not found or unauthorized" };
+    return { isSuccess: true, message: "Education record updated." };
+});
+
+
+const DeleteAsync = errorHandler(async function EducationService_DeleteAsync(data) {
+    const { userId, educationId } = data;
+    const deletedEducation = await models.Education.findOneAndDelete({ _id: educationId, userId });
+
+    if (!deletedEducation) return { isSuccess: false, message: "Education record not found or unauthorized" };
+    return { isSuccess: true, message: "Education record deleted successfully" };
+});
 
 
 export default {
-
-}
+    CreateAsync,
+    GetAllByUserIdAsync,
+    UpdateAsync,
+    DeleteAsync,
+};
