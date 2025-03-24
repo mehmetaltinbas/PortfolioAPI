78801905256436ae5922c1ad3ea1484fbe965f6f
altnbsmehmet <altnbsmehmet@icloud.com>
2025-03-24 03:52:06 +0300

    CRUD endpoints added for Project entity.

--- Controllers/ProjectController.js
+++ Controllers/ProjectController.js
@@ -5,5 +5,39 @@ import authMiddleware from "../middlewares/AuthMiddleware.js";
 const router = express.Router();
 
 
+router.post('/create', authMiddleware, async function CreateProject(req, res) {
+    req.body.userId = req.user.userId;
+    const creationResponse = await projectService.CreateAsync(req.body);
+    res.json(creationResponse);
+});
+
+
+router.get('/:userId', async function GetAllProjectsByUserId(req, res) {
+    const projects = await projectService.GetAllByUserIdAsync(req.params.userId);
+    res.json(projects);
+});
+
+
+router.patch('/update/:projectId', authMiddleware, async function UpdateProject(req, res) {
+    const data = { 
+        userId: req.user.userId, 
+        projectId: req.params.projectId, 
+        title: req.body.title, 
+        description: req.body.description, 
+        repositoryLinks: req.body.repositoryLinks, 
+        liveDemoLink: req.body.liveDemoLink, 
+        technologies: req.body.technologies 
+    };
+    const updateResponse = await projectService.UpdateAsync(data);
+    res.json(updateResponse);
+});
+
+
+router.delete('/delete/:projectId', authMiddleware, async function DeleteProject(req, res) {
+    const data = { userId: req.user.userId, projectId: req.params.projectId };
+    const deleteResponse = await projectService.DeleteAsync(data);
+    res.json(deleteResponse);
+});
+
 
 export default router;
--- Services/ProjectService.js
+++ Services/ProjectService.js
@@ -2,9 +2,44 @@ import { models } from '../data/db.js';
 import { errorHandler } from '../utilities/ErrorHandler.js';
 
 
+const CreateAsync = errorHandler(async function ProjectService_CreateAsync(data) {
+    const project = new models.Project(data);
+    await project.save();
+    return { isSuccess: true, message: "Project created successfully" };
+});
 
+
+const GetAllByUserIdAsync = errorHandler(async function ProjectService_GetAllByUserIdAsync(userId) {
+    const projects = await models.Project.find({ userId });
+    return { isSuccess: true, message: "Projects associated with given userId read.", projects };
+});
+
+
+const UpdateAsync = errorHandler(async function ProjectService_UpdateAsync(data) {
+    const { userId, projectId, ...updateFields } = data;
+    const updatedProject = await models.Project.findOneAndUpdate(
+        { _id: projectId, userId },
+        { $set: updateFields },
+        { new: true }
+    );
+
+    if (!updatedProject) return { isSuccess: false, message: "Project not found or unauthorized" };
+    return { isSuccess: true, message: "Project updated." };
+});
+
+
+const DeleteAsync = errorHandler(async function ProjectService_DeleteAsync(data) {
+    const { userId, projectId } = data;
+    const deletedProject = await models.Project.findOneAndDelete({ _id: projectId, userId });
+
+    if (!deletedProject) return { isSuccess: false, message: "Project not found or unauthorized" };
+    return { isSuccess: true, message: "Project deleted successfully" };
+});
 
 
 export default {
-
-}
+    CreateAsync,
+    GetAllByUserIdAsync,
+    UpdateAsync,
+    DeleteAsync,
+};
