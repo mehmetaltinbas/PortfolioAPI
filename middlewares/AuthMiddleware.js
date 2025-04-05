623c72706cd3d0f9f1bef7db920192d9d67975c9
altnbsmehmet <altnbsmehmet@icloud.com>
2025-04-05 15:48:56 +0300

    more flexible authmiddleware

--- middlewares/AuthMiddleware.js
+++ middlewares/AuthMiddleware.js
@@ -1,10 +1,11 @@
 import jwt from 'jsonwebtoken';
 
 export default function authMiddleware(req, res, next) {
-    const token = req.cookies.jwt || (req.headers.authorization && req.headers.authorization?.split(" ")[1]);
+    const token = req.cookies.jwt || req.cookies._vercel_jwt || (req.headers.authorization && req.headers.authorization.split(" ")[1]);
     /*console.log(`\nToken --> ${token}\n
-        Cookie --> ${req.cookies.jwt}\n
-        Header --> ${req.headers.authorization}\n`);*/
+            Cookie --> ${req.cookies.jwt}\n
+            Header --> ${req.headers.authorization}\n`);*/
+
     if (!token) return res.json({ isSuccess: false, message: "Unauthorized: No token provided" });
 
     jwt.verify(token, process.env.JWT_SIGNATURE, (err, decoded) => {
