e3057a46d450b141271129faebd58e037416762c
altnbsmehmet <altnbsmehmet@icloud.com>
2025-04-07 02:56:07 +0300

    some fixes

--- data/entities/Contact.js
+++ data/entities/Contact.js
@@ -4,7 +4,7 @@ const contactSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     type: { 
         type: String,
-        enum: ['email', 'phone', 'linkedin', 'github', 'twitter'],
+        enum: ['cv', 'email', 'phone', 'linkedin', 'github', 'twitter', 'upwork', 'instagram', 'youtube'],
         required: true,
      },
     value: { type: String, required: true },
